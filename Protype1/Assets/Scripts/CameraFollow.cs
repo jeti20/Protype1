07fa752d7834f48d817d4f1c79a1835ec7d2374c
Yeti <61649661+jeti20@users.noreply.github.com>
2022-10-01 13:44:36 +0200

    Update CameraFollow.cs

--- Protype1/Assets/Scripts/CameraFollow.cs
+++ Protype1/Assets/Scripts/CameraFollow.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(0, 0, 0);
+    private Vector3 offset1 = new Vector3(0, 5, -7);
+    private Vector3 offset2 = new Vector3(0, 2, 1);
     // Start is called before the first frame update
     void Start()
     {
+        offset = offset1;
+
         
     }
 
-    // LateUpdate funkcja jest wywo³ywana
+    // LateUpdate funkcja jest wywoÅ‚ywana
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
-        //transform.position = --> pozycja obiektu na którym jest skrypt
+        //transform.position = --> pozycja obiektu na ktÃ³rym jest skrypt
         // player.transform.position --> pozycja obiektu (zmiennej player)
-        //offset --> zmienna Vectro3(0, 5, -7) która sprawia, ¿e jest dodawana do obecnej pozycji gracza, dziêki czemu kamera jest lekko za graczem
+        //offset --> zmienna Vectro3(0, 5, -7) ktÃ³ra sprawia, Å¼e jest dodawana do obecnej pozycji gracza, dziÄ™ki czemu kamera jest lekko za graczem
+        
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            offset = offset2;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            offset = offset1;
+        }
     }
+
 }
